1dd378e4102a3f2961d87e536d2f94130c0d0d24
Trish Griego <pmgriego@gmail.com>
2015-12-10 12:58:02 -0800

    Added significantly more css, some aspects still in progress.

---
+++ chocolatepizza.js
@@ -0,0 +1,20 @@
+// This is where we wanted to go with JS code. We realzie we need an event listener. Yet we decided it would be better and more efficent to spread out time more wisely. As this was stretch goal. 
+
+
+// var checkedBox;
+// var uncheckedBox;
+// var userForm = document.getElementById('box');
+//
+// userFrom.addEventListener(checked);
+//
+//
+// var checkedBox function () {
+//
+// }
+//
+//
+// if () {
+//
+// } else {
+//
+// }
